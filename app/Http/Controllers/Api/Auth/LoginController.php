2876551a3b46f1a677ad7ac5dd1a4c5d28dfe328
Remedios Ortiz Martinez <remediosortizmartinez@gmail.com>
2018-04-02 02:05:07 -0500

    Initial commit

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    
+    public function login(Request $request) {
+
+    }
+
+    public function refresh(Request $request) {
+
+    }
+    
+}
